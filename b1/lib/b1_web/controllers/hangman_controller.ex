20fc1cd45915ce0b12cb3aa16d003aabd6dbc7a7
tapankumar23 <sabat.tapan@gmail.com>
2022-02-10 00:41:12 +0800

    hangman web

--- b1/lib/b1_web/controllers/hangman_controller.ex
+++ b1/lib/b1_web/controllers/hangman_controller.ex
@@ -1,9 +1,7 @@
 defmodule B1Web.HangmanController do
   use B1Web, :controller
 
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
+  def index(conn, _params), do: render(conn, "index.html")
 
   def new(conn, _params) do
     game = Hangman.new_game()
@@ -14,7 +12,6 @@ defmodule B1Web.HangmanController do
 
   def update(conn, params) do
     guess = params["make_move"]["guess"]
-
     put_in(conn.params["make_move"]["guess"], "")
     |> get_session(:game)
     |> Hangman.make_move(guess)
@@ -26,8 +23,9 @@ defmodule B1Web.HangmanController do
     tally =
       conn
       |> get_session(:game)
-      |> Hangman.make_move(guess)
+      |> Hangman.tally()
 
     render(conn, "game.html", tally: tally)
   end
+
 end
